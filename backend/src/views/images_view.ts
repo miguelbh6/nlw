a8205f149e35bbefdfb902d5a762cf6ae7d1b709
Francisco Miguel <miguelbh6@gmail.com>
2020-10-19 17:15:17 -0300

    primeiro commit

---
+++ backend/src/views/images_view.ts
@@ -0,0 +1,15 @@
+import Image from '../database/models/Images'
+
+export default {
+
+    render(image: Image) {
+        return {
+            id: image.id,
+            url: `http://192.168.1.108:3333/uploads/${image.path}`,
+        };
+    },
+
+    renderMany(images: Image[]){
+        return images.map(image => this.render(image))
+    }
+}
